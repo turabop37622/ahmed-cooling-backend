b6fe69148b5cae7be60afe4ffdbfaa48f56bc81e
Tauseef <tauseef.ahmad@innov8.jp>
2026-02-20 12:33:56 +0500

    fix: OTP email verification

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -7,8 +7,8 @@ const sendEmail = async (to, otp) => {
 
   const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
